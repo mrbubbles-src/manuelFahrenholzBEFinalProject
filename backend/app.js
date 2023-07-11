93bf15e44215163b4b1cd9f8995f33cfafae8c92
mrbubbles-src <feaver5683@gmail.com>
2023-07-11 15:27:13 +0200

    updated

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,35 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoCluster = process.env.MONGO_CLUSTER;
+const mongoDatabase = process.env.MONGO_DATABASE;
+
+mongoose
+    .connect(
+        `mongodb+srv://${mongoUser}:${mongoPassword}@${mongoCluster}/${mongoDatabase}?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        console.log("Connected to BooktDB");
+    })
+    .catch((error) => {
+        console.error("BooktDB connection error:", error);
+    });
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
