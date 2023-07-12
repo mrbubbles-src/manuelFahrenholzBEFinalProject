9732fb5aa79a9666cfb1543191d7134896d898ea
jacqueline-s-w <https://github.com/jacqueline-s-w/git-liste.git>
2023-07-12 18:05:03 +0200

    starting model

---
+++ backend/model/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const { UserRoles } = require("../lib/security/roles");
+const { userNotFound } = require("../middleware/errorHandler");
+const User = require("./userSchema");
+
+async function createUser(userData) {
+    return await User.create(userData);
+}
+
+async function authenticateUser(username, password) {
+    const user = await User.findOne({ username });
+    if (!user) {
+        return null;
+    }
+}
+
+/* Alle User anzeigen*/
+// async function findAllUsers() {
+//     return await User.find({});
+// }
+
+/* einzelne User anzeigen */
+// async function findSingleUser(id) {
+//     await userNotFound(User, id);
+//     return await User.findById(id);
+// }
+
+async function updateUser(id, data) {
+    await userNotFound(User);
+    return await User.findOneAndUpdate({ _id: id }, data, { new: true });
+}
+
+async function deleteUser(id) {
+    const user = await userNotFound(User, id);
+
+    if (user.role !== UserRoles.ADMIN) {
+        const error = new Error("Unzurreichende Berechtigungen");
+        error.statusCode = 403;
+        throw error;
+    }
+    await User.findOneAndDelete({ _id: id });
+}
+
+module.exports = {
+    User,
+    createUser,
+    authenticateUser,
+    // findAllUsers,
+    // findSingleUser,
+    updateUser,
+    deleteUser,
+};
