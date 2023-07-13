aacb2a4f568580a23c11684bd920ad1ad986f088
mrbubbles-src <feaver5683@gmail.com>
2023-07-13 12:03:16 +0200

    updated routes Co-authored-by: Manuel Fahrenholz <feaver5683@gmail.com>

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const {
+    httpCreateUser,
+    httpAuthenticateUser,
+    httpUpdateUser,
+    httpDeleteUser,
+} = require("../controller/user.controller");
+
+const { userValidationRules } = require("../lib/inputValidation/userRules");
+
+const { validateInputs } = require("../middleware/inputValidation");
+
+const { authenticateToken } = require("../middleware/userValidation");
+
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
+router.get("/", authenticateToken, function (req, res, next) {
     res.send("Welcome to the bookt server");
 });
 
-router.post("signup", validateInputs);
+router.post(
+    "signup",
+    validateInputs(userValidationRules.signup),
+    httpCreateUser
+);
+router.post(
+    "/login",
+    validateInputs(userValidationRules.login),
+    httpAuthenticateUser
+);
+router
+    .use(authenticateToken)
+    .route("/:id")
+    .put(httpUpdateUser)
+    .delete(httpDeleteUser);
+
 module.exports = router;
