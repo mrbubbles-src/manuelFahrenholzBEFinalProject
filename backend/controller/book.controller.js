f85c1d718f02691afad7eeabd964db625c2089d2
mrbubbles-src <feaver5683@gmail.com>
2023-07-13 14:13:36 +0200

    changed function name from createBook to saveBook Co-authored-by: mrbubbles-src <feaver5683@gmail.com> Co-authored-by: jacqueline-s-w <113904893+jacqueline-s-w@users.noreply.github.com>

--- backend/controller/book.controller.js
+++ backend/controller/book.controller.js
@@ -12,4 +12,28 @@ async function httpSaveBook(req, res, next) {
     }
 }
 
-module.exports = { httpSaveBook };
+async function httpGetAllBooks(req, res) {
+    try {
+        const books = await getAllBooks();
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(200).json([]);
+    }
+}
+
+async function httpDeleteBook(req, res, next) {
+    const { id } = req.params;
+    try {
+        const deletedBook = await deleteBook(id);
+        res.status(204).json({
+            message: "Buchdaten wurden gelöscht.",
+            deletedBook: deletedBook,
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: "Buch nicht gefunden",
+        });
+    }
+}
+
+module.exports = { httpSaveBook, httpGetAllBooks, httpDeleteBook };
